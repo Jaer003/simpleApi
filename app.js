0326e8bb1ac0eb3f02171e28602c0002c8d4d711
Jeremy Rollins <jeremy.rollins@endurance.com>
2019-11-20 09:41:18 -0700

    Intial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const bodyparser = require("body-parser");
+
+const port = process.env.PORT || 3200;
+
+const orders = [];
+
+// middleware
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.get("/get_orders", (req, res) => {
+    res.status(200).send("Working");
+});
+
+app.listen(port, () => {
+    console.log(`running on port ${port}`);
+});
